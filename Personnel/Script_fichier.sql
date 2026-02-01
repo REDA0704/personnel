dbaeef2140946e4003860bde96eb125863b015b5
Samircrypto67 <diampakaisaac@gmail.com>
2026-02-01 21:15:43 +0100

    Refactor SQL table creation scripts
    Updated table creation scripts with new naming conventions and added NOT NULL constraint for dateArrivee.

--- Personnel/Script_fichier.sql
+++ Personnel/Script_fichier.sql
@@ -1,27 +1,23 @@
+-- Script de création des tables
 
-- Script de création des tables
-
-
-
-CREATE TABLE Ligue (
+CREATE TABLE ligue (
     id_ligue INT AUTO_INCREMENT PRIMARY KEY,
     nom_ligue VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Employe (
+CREATE TABLE employe (
     id_employe INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     prenom VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    mot_de_passe VARCHAR(255) NOT NULL,
+    mail VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL,
-    date_de_depart DATE,
-    date_de_retour DATE,
+    dateArrivee DATE NOT NULL,
+    dateDepart DATE,
     id_ligue INT,
-  
+
     CONSTRAINT fk_employe_ligue
     FOREIGN KEY (id_ligue)
     REFERENCES ligue(id_ligue)
     ON DELETE SET NULL
 );
-
